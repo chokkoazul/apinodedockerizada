8f516b6d32a616cf76507fbfcea7f3df1969b1d8
Cristian Osorio <osoriocri@gmail.com>
2020-08-17 05:04:48 +0000

    modification deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-GOOGLE_PROJECT_ID=vivid-outcome-277104
-IMAGE_NAME=nodepocimagen 
-API_NAME=apinodedockerizada
+GOOGLE_PROJECT_ID=dockeruniversidades
+IMAGE_NAME=nodedockerizado
+SERVICE_NAME=servicenodedockerizada
 
 gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/$IMAGE_NAME:1.0 \
   --project=$GOOGLE_PROJECT_ID
 
-gcloud beta run deploy $API_NAME \
+gcloud beta run deploy $SERVICE_NAME \
   --image gcr.io/$GOOGLE_PROJECT_ID/$IMAGE_NAME:1.0 \
   --platform managed \
   --region us-central1 \
